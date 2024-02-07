ed67810ce4d166f095abbf64893f2ff4ecdc927d
cxldsakeb <jmarioulloav@gmail.com>
2024-02-06 22:42:27 -0600

    Practica 2 completa semana 4

---
+++ src/main/java/com/practica/m/PracticaHtmlApplication.java
@@ -0,0 +1,13 @@
+package com.practica.m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaHtmlApplication.class, args);
+	}
+
+}
